b0b57eee760297294aeec0663b618e7e5f14c9fc
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2024-08-18 23:37:56 +0200

    fixing an issue with allauth loading tag, because of returnig error 500 due to an uncorrect syntax; styling the plant list into cards and adding more user friendly style to the plant form

--- plants/urls.py
+++ plants/urls.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
 from plants import views
-from .views import AddPlantView
-from .views import AddPlantView, EditPlantView  
-
+from .views import AddPlantView, EditPlantView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Plant-related URLs
     path('plants/new', AddPlantView.as_view(), name='add_plant'),
     path('plants/todos', views.plant_and_todo_list, name='plant_and_todo_list'),
     path('todos/complete/<int:todo_id>', views.mark_todo_complete, name='mark_todo_complete'),
-    path('plants/<int:pk>/edit', EditPlantView.as_view(), name='edit_plant'), 
-    path('plants/<int:plant_id>/delete', views.delete_plant, name='delete_plant'), 
-    path('', views.home_view, name='home'),
+    path('plants/<int:pk>/edit', EditPlantView.as_view(), name='edit_plant'),
+    path('plants/<int:plant_id>/delete', views.delete_plant, name='delete_plant'),
     path('plants/', views.plant_list_view, name='plant_list'),
-    path('about/', views.about_view, name='about'), 
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('signup/', views.signup_view, name='signup'),
+
+    # Home and other informational pages
+    path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
     path('plant-guide/', views.plant_guide, name='plant-guide'),
-    path("accounts/", include("allauth.urls")), 
-     
+
+    # User authentication and signup
+    path('accounts/', include('django.contrib.auth.urls')),  # Django's built-in auth URLs
+    path('accounts/', include('allauth.urls')),  # Allauth for extended authentication features
+    path('signup/', views.signup_view, name='signup'),
 ]
